03bdf12d41acf966cfaaa6834d9407ec45f00cd1
yatiapriati7 <73220058+yatiapriati7@users.noreply.github.com>
2021-09-24 17:51:43 +0800

    Add files via upload

---
+++ Home_view.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html >
+<head>
+	 
+	<title>403 Forbidden</title>
+</head>
+<body bgcolor="yellow">
+
+<center> <?= $index; ?><br> 
+
+<a href="http://localhost/ci4/public/home/hobi">hobi</a>
+<a href="http://localhost/ci4/public/home/riwayat_pendidikan">riwayat pendidikan</a></center>
+
+</body>
+</html>
+
+
+   
