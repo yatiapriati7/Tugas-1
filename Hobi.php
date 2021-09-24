a2c148fed7a9ea7ade485fc57475287b9a7a50f2
yatiapriati7 <73220058+yatiapriati7@users.noreply.github.com>
2021-09-24 17:40:02 +0800

    Add files via upload

---
+++ Hobi.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>HOBI SAYA ADALAH</title>
+</head>
+<body bgcolor="blue">
+<p><?= $hobi ?></p>
+<center> 
+<a href="http://localhost/ci4/public/home/index">home</a>
+<a href="http://localhost/ci4/public/home/riwayat_pendidikan">riwayat pendidikan</a><h5>by yati apriati</h5><br></center>
+
+
+
+</body>
+</html>
--- riwayat_pendidikan.php
+++ riwayat_pendidikan.php
@@ -1,1 +1,12 @@
-// View Riwayat Pendidikan
+<!DOCTYPE html>
+<html>
+<head>
+	<title>riwayat pendidikan</title>
+</head>
+<body bgcolor="orange">
+	<center font-size:20px><?= $pendidikan; ?></center>
+	<center> 
+<a href="http://localhost/ci4/public/home/index">home</a>
+<a href="http://localhost/ci4/public/home/hobi">hobi</a></center>
+</body>
+</html>
